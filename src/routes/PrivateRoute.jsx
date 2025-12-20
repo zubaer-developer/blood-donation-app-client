71cd1a760caf693466f4dc3cdfb44af4b6da46f0
ZUBAER <zubaer.developer@gmail.com>
2025-12-20 21:25:06 +0600

    temporary private route page setup

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../components/shared/Loading";
 
-const PrivateRoute = () => {
-  return (
-    <div>
-      <h1>This is PrivateRoute Page.</h1>
-    </div>
-  );
+const PrivateRoute = ({ children }) => {
+  // Temporary - allow all access for now
+  const user = true;
+  const loading = false;
+  const location = useLocation();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRoute;
