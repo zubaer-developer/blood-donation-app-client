edea04987f4cb556d2ef16e9ca96450fea89ca04
ZUBAER <zubaer.developer@gmail.com>
2025-12-21 11:34:07 +0600

    update authprovider for registration, login, update profile and Track auth state changes

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,17 +1,69 @@
-import { createContext, useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { AuthContext } from "./AuthContext";
 
-export const AuthContext = createContext(null);
+// Initialize Firebase Auth
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // Placeholder auth info
+  // Create User (Registration)
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Sign In User (Login)
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Sign Out User (Logout)
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // Update User Profile
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  // Observer - For Track Auth State Changes
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("Current User:", currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  // Auth Information to be shared
   const authInfo = {
     user,
     loading,
-    setUser,
-    setLoading,
+    createUser,
+    signIn,
+    logOut,
+    updateUserProfile,
   };
 
   return (
